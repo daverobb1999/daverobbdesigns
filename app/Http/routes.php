ce2056d4bf3559f3649e1f885a04fba6f2d025be
David Robb <daverobb1999@gmail.com>
2016-03-09 19:50:02 -0500

    push 2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,5 +40,5 @@ Route::controllers([
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
-    Route::get('/home', 'HomeController@index');
+    Route::get('/dashboard', 'HomeController@index');
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,78 @@
-@extends('layouts.app')
-
-@section('content')
+<!doctype html>
+<html>
+<head>
+    @include('head')
+</head>
+<body>
 <div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in!
+    <header class="row">
+        @include('nav')
+    </header>
+
+    <div id="main" class="row">
+        <!-- /.row -->
+        @foreach($project as $item)
+            <div class="row">
+                <div class="col-md-7">
+                    <a href="">
+                        <img class="img-responsive border" src=http://localhost/drd_2/public/images/{{$item['url_to_image']}} alt="">
+                    </a>
+                </div>
+                <div class="col-md-5">
+                    <h3>{{$item['name']}}</h3>
+                    <p>{{$item['short_description']}}</p>
+                    <a class="btn btn-primary" href=http://localhost/drd_2/public/project/{{$item['id']}}>View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
                 </div>
             </div>
-        </div>
+            <!-- /.row -->
+
+            <hr>
+
+            @endforeach
+
+
+
+                    <!-- Pagination -->
+            <div class="row text-center">
+                <div class="col-lg-12">
+                    <ul class="pagination">
+                        <li>
+                            <a href="#">&laquo;</a>
+                        </li>
+                        <li class="active">
+                            <a href="#">1</a>
+                        </li>
+                        <li>
+                            <a href="#">2</a>
+                        </li>
+                        <li>
+                            <a href="#">3</a>
+                        </li>
+                        <li>
+                            <a href="#">4</a>
+                        </li>
+                        <li>
+                            <a href="#">5</a>
+                        </li>
+                        <li>
+                            <a href="#">&raquo;</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /.row -->
+
+            <hr>
+
     </div>
+
+    <footer class="row">
+        @include('footer')
+    </footer>
+
+
+
 </div>
-@endsection
+</body>
+</html>
